177f7905a97b686bb102bc391a333c5317a62d6b
wasniows <wasniows@gmail.com>
2022-12-27 20:15:06 +0100

    my data for firebird.utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,12 +10,12 @@ import {
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyDE47wOGzhBnvxj0_DRpmrxfNL0U5OtGts",
+  authDomain: "crwn-clothing-db-ee934.firebaseapp.com",
+  projectId: "crwn-clothing-db-ee934",
+  storageBucket: "crwn-clothing-db-ee934.appspot.com",
+  messagingSenderId: "1051138323053",
+  appId: "1:1051138323053:web:05490f7bd9a92fda110564",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
